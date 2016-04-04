cbc985f977a3abb021be3c2e7c9efa32eb0fb786
Pavel Alexeev <pahan@hubbitus.info>
2016-04-04 14:09:47 +0300

    In pg_stat_statements add human readable values

--- Postgres/pg_stat_statements.sql
+++ Postgres/pg_stat_statements.sql
@@ -3,8 +3,11 @@ SELECT
 	,d.datname
 	,s.query
 	,s.calls
-	,ROUND(s.total_time::numeric, 3) as total_time
-	,ROUND( (s.total_time / s.calls)::numeric, 3) as avg_query_time
+	,ROUND(s.total_time::numeric, 3) as total_time_ms
+	,ROUND((s.total_time / SUM(s.total_time) OVER() * 100)::numeric, 2) as "total_time %"
+	,( interval '1 millisecond' * ROUND(s.total_time::numeric) )::text as total_time_hr
+	,ROUND( (s.total_time / s.calls)::numeric, 3) as avg_query_time_ms
+	,( interval '1 millisecond' * ROUND( (s.total_time / s.calls)::numeric) )::text as avg_query_time_hr
 	,s.rows
 	,s.rows / s.calls as avg_rows
 	,s.shared_blks_hit
@@ -25,5 +28,10 @@ FROM
 	pg_stat_statements s
 	JOIN pg_authid a ON (a.oid = s.userid)
 	JOIN pg_database d ON (d.oid = s.dbid)
+WHERE
+	s.total_time >= 1 -- Longer 1 ms. To do not show SET, LOCL, DISCARD statements
 ORDER BY
-	avg_query_time DESC, total_time DESC
+	total_time_ms DESC, total_time DESC
+/
+
+SELECT pg_stat_statements_reset()
