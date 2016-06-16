9a3084b1dc5eb871022c3157f3fc31bbd87e5b25
Pavel Alexeev <pahan@hubbitus.info>
2016-06-16 15:11:57 +0300

    Add pg-indexes-size-usage-stat-bloat-missing.sql

---
+++ Postgres/pg-indexes-size-usage-stat-bloat-missing.sql
@@ -0,0 +1,145 @@
+-- https://wiki.postgresql.org/wiki/Index_Maintenance
+-- Index summary
+SELECT
+    pg_class.relname,
+    pg_size_pretty(pg_class.reltuples::BIGINT) AS rows_in_bytes,
+    pg_class.reltuples AS num_rows,
+    COUNT(indexname) AS number_of_indexes,
+    CASE WHEN x.is_unique = 1 THEN 'Y'
+       ELSE 'N'
+    END AS UNIQUE,
+    SUM(CASE WHEN number_of_columns = 1 THEN 1
+              ELSE 0
+            END) AS single_column,
+    SUM(CASE WHEN number_of_columns IS NULL THEN 0
+             WHEN number_of_columns = 1 THEN 0
+             ELSE 1
+           END) AS multi_column
+FROM pg_namespace 
+LEFT OUTER JOIN pg_class ON pg_namespace.oid = pg_class.relnamespace
+LEFT OUTER JOIN
+       (SELECT indrelid,
+           MAX(CAST(indisunique AS INTEGER)) AS is_unique
+       FROM pg_index
+       GROUP BY indrelid) x
+       ON pg_class.oid = x.indrelid
+LEFT OUTER JOIN
+    ( SELECT c.relname AS ctablename, ipg.relname AS indexname, x.indnatts AS number_of_columns FROM pg_index x
+           JOIN pg_class c ON c.oid = x.indrelid
+           JOIN pg_class ipg ON ipg.oid = x.indexrelid  )
+    AS foo
+    ON pg_class.relname = foo.ctablename
+WHERE 
+     pg_namespace.nspname='public'
+AND  pg_class.relkind = 'r'
+GROUP BY pg_class.relname, pg_class.reltuples, x.is_unique
+ORDER BY 2;
+
+-- Index size/usage statistics
+SELECT
+    t.tablename,
+    indexname,
+    c.reltuples AS num_rows,
+    pg_size_pretty(pg_relation_size(quote_ident(t.tablename)::text)) AS table_size,
+    pg_size_pretty(pg_relation_size(quote_ident(indexrelname)::text)) AS index_size,
+    CASE WHEN indisunique THEN 'Y'
+       ELSE 'N'
+    END AS UNIQUE,
+    idx_scan AS number_of_scans,
+    idx_tup_read AS tuples_read,
+    idx_tup_fetch AS tuples_fetched
+FROM pg_tables t
+LEFT OUTER JOIN pg_class c ON t.tablename=c.relname
+LEFT OUTER JOIN
+    ( SELECT c.relname AS ctablename, ipg.relname AS indexname, x.indnatts AS number_of_columns, idx_scan, idx_tup_read, idx_tup_fetch, indexrelname, indisunique FROM pg_index x
+           JOIN pg_class c ON c.oid = x.indrelid
+           JOIN pg_class ipg ON ipg.oid = x.indexrelid
+           JOIN pg_stat_all_indexes psai ON x.indexrelid = psai.indexrelid )
+    AS foo
+    ON t.tablename = foo.ctablename
+WHERE t.schemaname='public'
+ORDER BY 1,2;
+
+-- Duplicate indexes
+SELECT pg_size_pretty(SUM(pg_relation_size(idx))::BIGINT) AS SIZE,
+       (array_agg(idx))[1] AS idx1, (array_agg(idx))[2] AS idx2,
+       (array_agg(idx))[3] AS idx3, (array_agg(idx))[4] AS idx4
+FROM (
+    SELECT indexrelid::regclass AS idx, (indrelid::text ||E'\n'|| indclass::text ||E'\n'|| indkey::text ||E'\n'||
+                                         COALESCE(indexprs::text,'')||E'\n' || COALESCE(indpred::text,'')) AS KEY
+    FROM pg_index) sub
+GROUP BY KEY HAVING COUNT(*)>1
+ORDER BY SUM(pg_relation_size(idx)) DESC;
+
+-- Missing indexes?? Base: http://stackoverflow.com/questions/3318727/postgresql-index-usage-analysis
+SELECT
+	relname
+	,seq_scan - idx_scan AS too_much_seq
+	,CASE
+		WHEN seq_scan-idx_scan > 0
+		THEN 'Missing Index?'
+		ELSE 'OK'
+	END
+	, pg_size_pretty(pg_relation_size((schemaname || '.' || relname)::regclass)) AS rel_size
+	, seq_scan
+	, idx_scan
+FROM
+	pg_stat_all_tables
+WHERE
+	schemaname IN ('public', 'history')
+	AND pg_relation_size((schemaname || '.' || relname)::regclass) > 80000
+ORDER BY
+	too_much_seq DESC
+
+-- Index bloat http://pgsql.tapoueh.org/site/html/news/20080131.bloat.html
+--CREATE OR REPLACE VIEW bloat AS
+      SELECT
+        schemaname, tablename, reltuples::bigint, relpages::bigint, otta,
+        ROUND(CASE WHEN otta=0 THEN 0.0 ELSE sml.relpages/otta::numeric END,1) AS tbloat,
+        relpages::bigint - otta AS wastedpages,
+        bs*(sml.relpages-otta)::bigint AS wastedbytes,
+        pg_size_pretty((bs*(relpages-otta))::bigint) AS wastedsize,
+        iname, ituples::bigint, ipages::bigint, iotta,
+        ROUND(CASE WHEN iotta=0 OR ipages=0 THEN 0.0 ELSE ipages/iotta::numeric END,1) AS ibloat,
+        CASE WHEN ipages < iotta THEN 0 ELSE ipages::bigint - iotta END AS wastedipages,
+        CASE WHEN ipages < iotta THEN 0 ELSE bs*(ipages-iotta) END AS wastedibytes,
+        CASE WHEN ipages < iotta THEN pg_size_pretty(0::bigint) ELSE pg_size_pretty((bs*(ipages-iotta))::bigint) END AS wastedisize
+      FROM (
+        SELECT
+          schemaname, tablename, cc.reltuples, cc.relpages, bs,
+          CEIL((cc.reltuples*((datahdr+ma-
+            (CASE WHEN datahdr%ma=0 THEN ma ELSE datahdr%ma END))+nullhdr2+4))/(bs-20::float)) AS otta,
+          COALESCE(c2.relname,'?') AS iname, COALESCE(c2.reltuples,0) AS ituples, COALESCE(c2.relpages,0) AS ipages,
+          COALESCE(CEIL((c2.reltuples*(datahdr-12))/(bs-20::float)),0) AS iotta -- very rough approximation, assumes all cols
+        FROM (
+          SELECT
+            ma,bs,schemaname,tablename,
+            (datawidth+(hdr+ma-(case when hdr%ma=0 THEN ma ELSE hdr%ma END)))::numeric AS datahdr,
+            (maxfracsum*(nullhdr+ma-(case when nullhdr%ma=0 THEN ma ELSE nullhdr%ma END))) AS nullhdr2
+          FROM (
+            SELECT
+              schemaname, tablename, hdr, ma, bs,
+              SUM((1-null_frac)*avg_width) AS datawidth,
+              MAX(null_frac) AS maxfracsum,
+              hdr+(
+                SELECT 1+count(*)/8
+                FROM pg_stats s2
+                WHERE null_frac<>0 AND s2.schemaname = s.schemaname AND s2.tablename = s.tablename
+              ) AS nullhdr
+            FROM pg_stats s, (
+              SELECT
+                (SELECT current_setting('block_size')::numeric) AS bs,
+                CASE WHEN substring(v,12,3) IN ('8.0','8.1','8.2') THEN 27 ELSE 23 END AS hdr,
+                CASE WHEN v ~ 'mingw32' THEN 8 ELSE 4 END AS ma
+              FROM (SELECT version() AS v) AS foo
+            ) AS constants
+            GROUP BY 1,2,3,4,5
+          ) AS foo
+        ) AS rs
+        JOIN pg_class cc ON cc.relname = rs.tablename
+        JOIN pg_namespace nn ON cc.relnamespace = nn.oid AND nn.nspname = rs.schemaname
+        LEFT JOIN pg_index i ON indrelid = cc.oid
+        LEFT JOIN pg_class c2 ON c2.oid = i.indexrelid
+      ) AS sml
+      WHERE sml.relpages - otta > 0 OR ipages - iotta > 10
+      ORDER BY wastedbytes DESC, wastedibytes DESC;
