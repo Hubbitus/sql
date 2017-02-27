50be1e6646e6d47f1059f6b2da656a6c545b8ee4
Pavel Alexeev <pahan@hubbitus.info>
2017-02-27 13:23:15 +0300

    Add hostname.function.sql

---
+++ Postgres/hostname.function.sql
@@ -0,0 +1,7 @@
+﻿CREATE OR REPLACE FUNCTION drop_system_caches()
+RETURNS text AS
+$BODY$
+        return `sudo /bin/sync ; echo 3 | sudo /usr/bin/tee /proc/sys/vm/drop_caches ; sudo /bin/sync`;
+;
+$BODY$
+LANGUAGE 'plperlu';
