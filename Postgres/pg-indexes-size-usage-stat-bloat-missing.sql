d96f9421649e50f80c5498dd923ff677c792b554
Pavel Alexeev <pahan@hubbitus.info>
2017-06-28 19:27:24 +0300

    Add ordering by actual size

--- Postgres/pg-indexes-size-usage-stat-bloat-missing.sql
+++ Postgres/pg-indexes-size-usage-stat-bloat-missing.sql
@@ -82,8 +82,10 @@ SELECT
 		,idstat.relname AS table_name
 		,indexrelname AS index_name
 		,idstat.idx_scan AS times_used
-		,pg_size_pretty(pg_relation_size(idstat.schemaname || '.' || idstat.relname)) AS table_size
-		,pg_size_pretty(pg_relation_size(idstat.schemaname || '.' || indexrelname)) AS index_size
+		,pg_size_pretty(pg_relation_size(idstat.schemaname || '.' || idstat.relname)) AS table_size_human
+		,pg_size_pretty(pg_relation_size(idstat.schemaname || '.' || indexrelname)) AS index_size_human
+		,pg_relation_size(idstat.schemaname || '.' || idstat.relname) AS table_size
+		,pg_relation_size(idstat.schemaname || '.' || indexrelname) AS index_size
 		,n_tup_upd + n_tup_ins + n_tup_del as num_writes
 		,indexdef AS definition
 FROM pg_stat_user_indexes AS idstat
@@ -91,7 +93,7 @@ FROM pg_stat_user_indexes AS idstat
 		JOIN pg_stat_user_tables AS tabstat ON idstat.relname = tabstat.relname
 WHERE idstat.idx_scan < 200
 	AND indexdef !~* 'unique'
-ORDER BY idstat.relname, indexrelname;
+ORDER BY times_used, index_size DESC, idstat.relname, indexrelname;
 
 -- Duplicate indexes
 SELECT
