bff0d9a1616e1498f8e61e5d5d3f0e67704dba3e
Pavel Alexeev <pahan@hubbitus.info>
2018-09-18 20:43:13 +0300

    pg_stat_activity.sql: Add vacuum progress look example

--- Postgres/pg_stat_activity.sql
+++ Postgres/pg_stat_activity.sql
@@ -3,7 +3,11 @@
 	,to_char(CURRENT_TIMESTAMP - backend_start, 'DD HH24:MI:SS.MS') as client_conn_time
 	,to_char(CURRENT_TIMESTAMP - xact_start, 'DD HH24:MI:SS.MS') as transaction_time
 	,to_char(CURRENT_TIMESTAMP - state_change, 'DD HH24:MI:SS.MS') as state_change_time
-	,state, waiting, datname, pid, usename, application_name, client_addr, backend_start, xact_start, query_start, state_change, query
+	,state
+//	,waiting
+	,datname, pid
+	,query
+	,usename, application_name, client_addr, backend_start, xact_start, query_start, state_change
 --	,pg_terminate_backend(pid)
 --	,CASE
 --		WHEN CURRENT_TIMESTAMP - query_start > interval '1 minute'
@@ -15,7 +19,7 @@ WHERE
 	1=1
 	AND state NOT IN ('idle')
 	AND pid != pg_backend_pid()
---	AND state = 'active'
+	AND state = 'idle in transaction'
 --	AND '10.0.17.32' = client_addr
 ORDER BY
 --	query_time DESC
@@ -25,11 +29,13 @@ ORDER BY
 	transaction_time DESC, query_time DESC
 /
 
-SELECT pg_terminate_backend(7796)
+SELECT pg_terminate_backend(734)
 /
 
 REINDEX TABLE bo_document_base
 /
 
-VACUUM VERBOSE ANALYZE bo_document_base
+-- Vacuum progress
+-- https://www.dbrnd.com/2017/12/postgresql-check-the-progress-of-running-vacuum/
+SELECT * FROM pg_stat_progress_vacuum
 /
