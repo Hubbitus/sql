b0d21e9e80466c91958a7380645cda5d925b0bff
Pavel Alexeev <pahan@hubbitus.info>
2018-09-18 19:26:49 +0300

    Fix indexes and table sizes calculation

--- Postgres/pg-database-sizes.sql
+++ Postgres/pg-database-sizes.sql
@@ -35,6 +35,7 @@ SELECT
 /
 
 -- My extended variant with tables and indexes sizes:
+//CREATE TABLE tmp__t1 AS
 WITH rels AS(
 	SELECT
 		table_catalog, table_schema, table_name, table_type
@@ -48,19 +49,21 @@ WITH rels AS(
 		rels.relation
 	--	,relation::regclass::oid
 		,pg_size_pretty(pg_total_relation_size(relation::regclass::oid)) AS "total_size_pretty"
-		,pg_size_pretty(pg_relation_size(relation::regclass::oid)) AS "table_size_pretty"
+		,pg_size_pretty(pg_table_size(relation::regclass::oid)) AS "table_size_pretty"
 		,pg_total_relation_size(relation::regclass::oid) AS "total_size_bytes"
-		,pg_relation_size(relation::regclass::oid) AS "table_size_bytes"
+		,pg_table_size(relation::regclass::oid) AS "table_size_bytes"
+		,pg_indexes_size(relation::regclass) as "indexes_size_bytes"
+		,pg_size_pretty(pg_indexes_size(relation::regclass)) as "indexes_size_pretty"
 	FROM rels
 ), sizes_ext as (
 	SELECT
 		relation
 		,total_size_pretty
 		,table_size_pretty
-		,pg_size_pretty(total_size_bytes - table_size_bytes) as "indexes_size_pretty"
+		,indexes_size_pretty
 		,total_size_bytes
 		,table_size_bytes
-		,total_size_bytes - table_size_bytes AS "indexes_size_bytes"
+		,indexes_size_bytes
 	FROM sizes
 )
 SELECT *
@@ -81,6 +84,18 @@ ORDER BY
 	,indexes_size_bytes DESC
 /
 
+-- https://www.postgresql.org/docs/current/static/functions-admin.html
+SELECT
+	pg_size_pretty(pg_total_relation_size('history.logged_actions'::regclass))		as pg_total_relation_size	-- Total disk space used by the specified table, including all indexes and TOAST data
+	,pg_size_pretty(pg_indexes_size('history.logged_actions'::regclass))			as pg_indexes_size			-- Total disk space used by indexes attached to the specified table
+	,pg_size_pretty(pg_table_size('history.logged_actions'::regclass))				as pg_table_size			-- Disk space used by the specified table, excluding indexes (but including TOAST, free space map, and visibility map)
+	,'=>' as "=>"
+	,pg_size_pretty(pg_relation_size('history.logged_actions'::regclass))			as pg_relation_size			-- Shorthand for pg_relation_size(..., 'main') => Disk space used by the specified fork ('main', 'fsm', 'vm', or 'init') of the specified table or index
+	,pg_size_pretty(pg_relation_size('history.logged_actions'::regclass, 'main'))	as pg_relation_size__main	-- Disk space used by the specified fork ('main', 'fsm', 'vm', or 'init') of the specified table or index
+	,pg_size_pretty(pg_relation_size('history.logged_actions'::regclass, 'fsm'))	as pg_relation_size__fsm	-- Disk space used by the specified fork ('main', 'fsm', 'vm', or 'init') of the specified table or index
+	,pg_size_pretty(pg_relation_size('history.logged_actions'::regclass, 'vm'))		as pg_relation_size__vm		-- Disk space used by the specified fork ('main', 'fsm', 'vm', or 'init') of the specified table or index
+	,pg_size_pretty(pg_relation_size('history.logged_actions'::regclass, 'init'))	as pg_relation_size__init	-- Disk space used by the specified fork ('main', 'fsm', 'vm', or 'init') of the specified table or index
+/
 
 SELECT pg_table_is_visible('pgbench.public.pgbench_accounts'::regclass::oid)
 /
@@ -90,3 +105,4 @@ FROM (
 	SELECT SUM(pg_total_relation_size(C.oid)) as sum_size, pg_size_pretty(SUM(pg_total_relation_size(C.oid))) as sum_size_pretty FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) WHERE nspname NOT IN ('pg_catalog', 'information_schema') AND nspname || '.' || relname NOT IN ('history.logged_actions')
 )t
 /
+
