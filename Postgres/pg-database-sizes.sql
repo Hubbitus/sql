41cbc08c2874d438b66a64c2c50467be4068b7d5
Pavel Alexeev <Pavel_Alexeev@epam.com>
2021-05-31 11:07:06 +0300

    Add different table parts size details example
    By https://dba.stackexchange.com/questions/23879/measure-the-size-of-a-postgresql-table-row

--- Postgres/pg-database-sizes.sql
+++ Postgres/pg-database-sizes.sql
@@ -131,3 +131,30 @@ FROM meta_draft
 SELECT id, type, length(value) as value_length, pg_size_pretty(length(value)::bigint) as value_length_human, name, created_at, created_by, updated_at, updated_by, version, active
 FROM meta_draft
 WHERE version NOT IN (SELECT MAX(version) FROM meta_draft)
+
+
+-- Table parts size details
+-- https://dba.stackexchange.com/questions/23879/measure-the-size-of-a-postgresql-table-row
+SELECT l.metric, l.nr AS "bytes/ct"
+     , CASE WHEN is_size THEN pg_size_pretty(nr) END AS bytes_pretty
+     , CASE WHEN is_size THEN nr / NULLIF(x.ct, 0) END AS bytes_per_row
+FROM  (
+   SELECT min(tableoid)        AS tbl      -- = 'public.tbl'::regclass::oid
+        , count(*)             AS ct
+        , sum(length(t::text)) AS txt_len  -- length in characters
+   FROM   pg_catalog.pg_class t            -- provide table name *once*
+   ) x
+ , LATERAL (
+   VALUES
+      (true , 'core_relation_size'               , pg_relation_size(tbl))
+    , (true , 'visibility_map'                   , pg_relation_size(tbl, 'vm'))
+    , (true , 'free_space_map'                   , pg_relation_size(tbl, 'fsm'))
+    , (true , 'table_size_incl_toast'            , pg_table_size(tbl))
+    , (true , 'indexes_size'                     , pg_indexes_size(tbl))
+    , (true , 'total_size_incl_toast_and_indexes', pg_total_relation_size(tbl))
+    , (true , 'live_rows_in_text_representation' , txt_len)
+    , (false, '------------------------------'   , NULL)
+    , (false, 'row_count'                        , ct)
+    , (false, 'live_tuples'                      , pg_stat_get_live_tuples(tbl))
+    , (false, 'dead_tuples'                      , pg_stat_get_dead_tuples(tbl))
+   ) l(is_size, metric, nr);
