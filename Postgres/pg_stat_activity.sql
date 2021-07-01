aa5597a3e38845812d62bc17d9bdebabb0474d73
Pavel Alexeev <Pavel_Alexeev@epam.com>
2021-07-02 01:24:13 +0300

    pg_stat_activity add robust query parsing to show source relations

--- Postgres/pg_stat_activity.sql
+++ Postgres/pg_stat_activity.sql
@@ -1,7 +1,9 @@
-﻿SELECT
+SELECT
 	to_char(CURRENT_TIMESTAMP - query_start, 'DD HH24:MI:SS.MS') as query_time
+	,usename
+--	,(SELECT regexp_matches(query, 'SELECT.+?FROM\s+([\w\d.]+)(?:\s|$)', 'gi')) as query_from_naive
+	,epm_ddo_custom.get_query_tables(query) as query_from
 	,query
-	,ARRAY(SELECT regexp_matches(query, 'SELECT.+?FROM\s+([\w\d.]+)(?:\s|$)', 'gi')) as query_from
 	,to_char(CURRENT_TIMESTAMP - backend_start, 'DD HH24:MI:SS.MS') as client_conn_time
 	,to_char(CURRENT_TIMESTAMP - xact_start,    'DD HH24:MI:SS.MS') as transaction_time
 	,to_char(CURRENT_TIMESTAMP - state_change,  'DD HH24:MI:SS.MS') as state_change_time
@@ -10,7 +12,7 @@
 	,wait_event_type
 	,datname, pid
 	,application_name
-	,usename, client_addr, backend_start, xact_start, query_start, state_change
+	,client_addr, backend_start, xact_start, query_start, state_change
 --	,pg_terminate_backend(pid) -- KILL ALL! Be carefull
 --	,CASE
 --		WHEN CURRENT_TIMESTAMP - query_start > interval '1 minute'
@@ -35,7 +37,31 @@ ORDER BY
 
 SELECT pg_terminate_backend(17803)
 
+SHOW track_activity_query_size
+
 -- Vacuum progress
 -- https://www.dbrnd.com/2017/12/postgresql-check-the-progress-of-running-vacuum/
 SELECT * FROM pg_stat_progress_vacuum
 ;
+
+-- Function to extract used in query relations (tables, views...). That use explain, so should work on any queries by complexity!
+-- By https://stackoverflow.com/a/44811746/307525 but extended to exceptions handling by truncated queries (when track_activity_query_size set to small value)
+-- Example of invocation: SELECT epm_ddo_custom.get_query_tables('SELECT * FROM pg_catalog.pg_class')
+CREATE OR REPLACE FUNCTION epm_ddo_custom.get_query_tables(_query text)
+RETURNS text[]
+LANGUAGE plpgsql AS $$
+DECLARE
+	x_ xml;
+	err_text_ text;
+	err_detail_ text;
+	err_hint_ text;
+BEGIN
+	EXECUTE 'explain (format xml) ' || _query INTO x_;
+	RETURN xpath('//explain:Relation-Name/text()', x_, ARRAY[ARRAY['explain', 'http://www.postgresql.org/2009/explain']])::text[];
+EXCEPTION WHEN OTHERS THEN
+	GET STACKED DIAGNOSTICS err_text_   = MESSAGE_TEXT,
+				err_detail_ = PG_EXCEPTION_DETAIL,
+				err_hint_   = PG_EXCEPTION_HINT;
+	RETURN ARRAY['ERROR: ' || err_text_, err_detail_, err_hint_];
+END $$;
+
