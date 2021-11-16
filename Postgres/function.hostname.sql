656a0d61ddae6038e8f8c723f6f9e21ef9a53dce
Pavel Alexeev <Pavel_Alexeev@epam.com>
2021-11-16 18:02:27 +0300

    hostname function - fix name, add link to implementation suggestion

--- Postgres/function.hostname.sql
+++ Postgres/function.hostname.sql
@@ -1,4 +1,5 @@
-﻿CREATE OR REPLACE FUNCTION drop_system_caches()
+/** By https://www.postgresql.org/message-id/292551.66852.qm%40web23604.mail.ird.yahoo.com **/
+CREATE OR REPLACE FUNCTION hostname()
 RETURNS text AS
 $BODY$
         return `hostname`;
